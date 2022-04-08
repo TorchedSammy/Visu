84a2d5ed3b28e413cc513d6d57ff67a19b3fbe3a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-04-08 17:17:00 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Visu
+> Audio visualizer for Lite XL.
+
+This is a little demo audio visualizer for the Lite XL. You don't want this (yet),
+there's quite a few (2) problems.
+
+# Installation
+But if you still want it anyway, just git clone into your plugins folder.
+Requires `cava` and does not work on Windows.
+
+# License
+MIT
+
---
+++ init.lua
@@ -0,0 +1,58 @@
+-- mod-version:2 -- lite-xl 2.0
+local core = require 'core'
+local style = require 'core.style'
+local RootView = require 'core.rootview'
+
+local barsNumber = 32
+local confFormat = [[
+[general]
+bars = %d
+
+[output]
+method = raw
+raw_target = %s
+bit_format = %s
+]]
+
+local cavaConf = confFormat:format(barsNumber, '/dev/stdout', '16bit')
+local tmp = core.temp_filename('cavaconf', '/tmp')
+
+do
+	local f <close> = io.open(tmp, 'w')
+	f:write(cavaConf)
+end
+local proc = process.start {'cava', '-p', tmp}
+
+
+local chunkSize = 2 * barsNumber
+local byteFormat = 'H'
+local byteMax = 65535
+
+local function getLatestInfo()
+	local data = proc:read_stdout(chunkSize)
+	if data:len() < chunkSize then return nil end
+	local fmt = byteFormat:rep(barsNumber)
+	local bars = table.pack(string.unpack(fmt, data))
+
+	for i, b in ipairs(bars) do
+		bars[i] = b / byteMax
+	end
+
+	return bars
+end
+
+local rvDraw = RootView.draw
+function RootView:draw(...)
+	rvDraw(self, ...)
+
+	if core.active_view == core.command_view then return end
+	local w = 10 * SCALE
+	local bars = getLatestInfo()
+	if bars ~= nil then
+		for i = 1, barsNumber do
+			core.log_quiet(tostring(bars[i]))
+			local h = (bars[i] * 80) * SCALE
+			renderer.draw_rect(self.size.x - (30 * i), self.size.y - core.status_view.size.y - h - (2 * SCALE), w, h, style.normal)
+		end
+	end
+end
